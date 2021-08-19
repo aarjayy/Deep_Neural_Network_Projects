a17ddd86eee576d4a788d11db741c3d02935109b
Roshan Jangid <81475333+aarjayy@users.noreply.github.com>
2021-08-19 17:26:56 +0530

    Add files via upload

---
+++ Python files/transformer_layers.py
@@ -0,0 +1,220 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+
+"""
+This file defines layer types that are commonly used for transformers.
+"""
+
+class PositionalEncoding(nn.Module):
+    """
+    Encodes information about the positions of the tokens in the sequence. In
+    this case, the layer has no learnable parameters, since it is a simple
+    function of sines and cosines.
+    """
+    def __init__(self, embed_dim, dropout=0.1, max_len=5000):
+        """
+        Construct the PositionalEncoding layer.
+
+        Inputs:
+         - embed_dim: the size of the embed dimension
+         - dropout: the dropout value
+         - max_len: the maximum possible length of the incoming sequence
+        """
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        assert embed_dim % 2 == 0
+        # Create an array with a "batch dimension" of 1 (which will broadcast
+        # across all examples in the batch).
+        pe = torch.zeros(1, max_len, embed_dim)
+        ############################################################################
+        # TODO: Construct the positional encoding array as described in            #
+        # Transformer_Captioning.ipynb.  The goal is for each row to alternate     #
+        # sine and cosine, and have exponents of 0, 0, 2, 2, 4, 4, etc. up to      #
+        # embed_dim. Of course this exact specification is somewhat arbitrary, but #
+        # this is what the autograder is expecting. For reference, our solution is #
+        # less than 5 lines of code.                                               #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        position_id = torch.arange(0,max_len).unsqueeze(1)
+        freqencies = torch.pow(10000.,-torch.arange(0,embed_dim,2, dtype = torch.float)/embed_dim)
+        pe[:,:,0::2] = torch.sin(position_id*freqencies)
+        pe[:,:,1::2] = torch.cos(position_id*freqencies)
+        pass
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        # Make sure the positional encodings will be saved with the model
+        # parameters (mostly for completeness).
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Element-wise add positional embeddings to the input sequence.
+
+        Inputs:
+         - x: the sequence fed to the positional encoder model, of shape
+              (N, S, D), where N is the batch size, S is the sequence length and
+              D is embed dim
+        Returns:
+         - output: the input sequence + positional encodings, of shape (N, S, D)
+        """
+        N, S, D = x.shape
+        # Create a placeholder, to be overwritten by your code below.
+        output = torch.empty((N, S, D))
+        ############################################################################
+        # TODO: Index into your array of positional encodings, and add the         #
+        # appropriate ones to the input sequence. Don't forget to apply dropout    #
+        # afterward. This should only take a few lines of code.                    #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        assert x.ndim == 3 and x.shape[-1] == self.pe.shape[2]
+        #print(x.shape[-1])
+        positional_encoding = self.pe[:,:x.shape[1],:]
+        #print(positional_encoding.shape)
+        output = positional_encoding + x
+        output = self.dropout(output)
+        pass
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+        return output
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    A model layer which implements a simplified version of masked attention, as
+    introduced by "Attention Is All You Need" (https://arxiv.org/abs/1706.03762).
+
+    Usage:
+      attn = MultiHeadAttention(embed_dim, num_heads=2)
+
+      # self-attention
+      data = torch.randn(batch_size, sequence_length, embed_dim)
+      self_attn_output = attn(query=data, key=data, value=data)
+
+      # attention using two inputs
+      other_data = torch.randn(batch_size, sequence_length, embed_dim)
+      attn_output = attn(query=data, key=other_data, value=other_data)
+    """
+
+    def __init__(self, embed_dim, num_heads, dropout=0.1):
+        """
+        Construct a new MultiHeadAttention layer.
+
+        Inputs:
+         - embed_dim: Dimension of the token embedding
+         - num_heads: Number of attention heads
+         - dropout: Dropout probability
+        """
+        super().__init__()
+        assert embed_dim % num_heads == 0
+
+        # We will initialize these layers for you, since swapping the ordering
+        # would affect the random number generation (and therefore your exact
+        # outputs relative to the autograder). Note that the layers use a bias
+        # term, but this isn't strictly necessary (and varies by
+        # implementation).
+        self.key = nn.Linear(embed_dim, embed_dim)
+        self.query = nn.Linear(embed_dim, embed_dim)
+        #self.key = nn.Linear(embed_dim, embed_dim)
+        self.value = nn.Linear(embed_dim, embed_dim)
+        self.proj = nn.Linear(embed_dim, embed_dim)
+        self.head_dimension = int(embed_dim/num_heads)
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads 
+        
+        ############################################################################
+        # TODO: Initialize any remaining layers and parameters to perform the      #
+        # attention operation as defined in Transformer_Captioning.ipynb. We will  #
+        # also apply dropout just after the softmax step. For reference, our       #
+        # solution is less than 5 lines.                                           #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        self.attention_dropout = nn.Dropout(p=dropout)
+        self.softmax = nn.Softmax(dim=-1)
+    def attention(self, query, key, value, mask=None):
+      scores = torch.matmul(query, key.transpose(-2,-1))/math.sqrt(self.head_dimension)
+      if mask is not None:
+        scores.masked_fill_(mask == torch.tensor(False), float("-inf"))
+      attention_weights = self.softmax(scores)
+      attention_weights = self.attention_dropout(attention_weights)
+      int_token_represent = torch.matmul(attention_weights, value)
+
+      return int_token_represent, attention_weights   
+
+      pass
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+    def forward(self, query, key, value, attn_mask=None):
+        """
+        Calculate the masked attention output for the provided data, computing
+        all attention heads in parallel.
+
+        In the shape definitions below, N is the batch size, S is the source
+        sequence length, T is the target sequence length, and E is the embedding
+        dimension.
+
+        Inputs:
+        - query: Input data to be used as the query, of shape (N, S, E)
+        - key: Input data to be used as the key, of shape (N, T, E)
+        - value: Input data to be used as the value, of shape (N, T, E)
+        - attn_mask: Array of shape (T, S) where mask[i,j] == 0 indicates token
+          i in the target should not be influenced by token j in the source.
+
+        Returns:
+        - output: Tensor of shape (N, S, E) giving the weighted combination of
+          data in value according to the attention weights calculated using key
+          and query.
+        """
+        N, S, D = query.shape
+        N, T, D = value.shape
+        batch_size = N
+        # Create a placeholder, to be overwritten by your code below.
+        output = torch.empty((N, T, D))
+        ############################################################################
+        # TODO: Implement multiheaded attention using the equations given in       #
+        # Transformer_Captioning.ipynb.                                            #
+        # A few hints:                                                             #
+        #  1) You'll want to split your shape from (N, T, E) into (N, T, H, E/H),  #
+        #     where H is the number of heads.                                      #
+        #  2) The function torch.matmul allows you to do a batched matrix multiply.#
+        #     For example, you can do (N, H, T, E/H) by (N, H, E/H, T) to yield a  #
+        #     shape (N, H, T, T). For more examples, see                           #
+        #     https://pytorch.org/docs/stable/generated/torch.matmul.html          #
+        #  3) For applying attn_mask, think how the scores should be modified to   #
+        #     prevent a value from influencing output. Specifically, the PyTorch   #
+        #     function masked_fill may come in handy.                              #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+      #batch_size = N
+        num_heads = self.num_heads
+        embed_dim = self.embed_dim
+        head_dimension = self.head_dimension
+        #query, key, value = [net(x).view(batch_size, -1, self.number_of_heads, self.head_dimension).transpose(1, 2)]
+        query = self.query(query).view(batch_size,-1,num_heads,head_dimension).transpose(1,2)
+        key = self.key(key).view(batch_size,-1,num_heads,head_dimension).transpose(1,2)
+        value = self.value(value).view(batch_size,-1,num_heads,head_dimension).transpose(1,2)
+        int_token_represent, attention_weights = self.attention(query,key,value,attn_mask)
+        reshaped = int_token_represent.transpose(1,2).reshape(batch_size,-1,self.num_heads*self.head_dimension)
+        output = self.proj(reshaped)
+
+        pass
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+        return output
+
+
